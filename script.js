668d20678a66ad2d9eea394806467b9f755406c0
Alexandre F. Costa <fcosta.xand@gmail.com>
2023-10-11 14:53:54 -0300

    feat add apple generation and loop

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-$("#board").width($("body").innerWidth() * 0.8);
-$("#board").height($("body").innerHeight() * 0.8);
+$(document).ready(() => {
+    boardSize = 500;
+    blockSize = $("#board").width() / 20;
+    canvasContext = $("#board")[0].getContext("2d");
 
-canvas_context = $("#board")[0].getContext("2d");
-canvas_context.fillStyle = "gray";
-canvas_context.fillRect(0, 0, $("#board").innerWidth(), $("#board").innerHeight());
+    $("#board").attr("width", "500px");
+    $("#board").attr("height", "500px");
+    paintHoleBoard();
+
+    setInterval(() => {
+        paintHoleBoard();
+
+        appleX = randomIntFromInterval(0, 19);
+        appleY = randomIntFromInterval(0, 19);
+
+        createApple(appleX, appleY);
+    }, 1000);
+})
+
+function paintHoleBoard(){
+    canvasContext.fillStyle = "gray";
+    canvasContext.fillRect(0, 0, $("#board").width(), $("#board").height());
+}
+
+function createApple(posX, posY){
+    realX = posX * blockSize;
+    realY = posY * blockSize;
+
+    canvasContext.fillStyle = "#ff0000";
+    canvasContext.fillRect(realX, realY, blockSize, blockSize);
+}
+
+function randomIntFromInterval(min, max){ // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
